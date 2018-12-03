d0e3e875fa8ed83089752b3524278a241b0b3905
AndreyKrotkikh <andrei.krotkih@gmail.com>
2018-12-03 22:22:00 +0500

    Gallery fix
    Убрал необходимость передачи в _render() значения адреса картинки.

--- scripts/phones/components/phone-viewer.js
+++ scripts/phones/components/phone-viewer.js
@@ -26,7 +26,8 @@ export default class PhoneViewer extends Component {
         // console.log(thumbsClick.getAttribute('src'));
         /*this._trigger('add', this._phone.dataset.phoneId);*/
         // this._trigger('add',this._phone.id);
-        this._render(thumbsClick.getAttribute('src'));
+        this._imgSrc = thumbsClick.getAttribute('src');
+        this._render();
     })
   }
 
@@ -37,11 +38,14 @@ export default class PhoneViewer extends Component {
     super.show();
   }
 
-  _render(_imgSrc) {
+  _render() {
     const { _phone: phone } = this;
+    if (!this._imgSrc) {
+      this._imgSrc = phone.images[0];
+    }
 
     this._element.innerHTML = `
-      <img class="phone" src="${_imgSrc}">
+      <img class="phone" src="${this._imgSrc}">
 
     <button data-element="button-back">Back</button>
     <button data-element="button-basket">Add to basket</button>
